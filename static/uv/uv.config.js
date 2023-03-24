68c638b733dbfb2a6b298c5b22d8dab101022209
TheTIW <128273154+TheTIW@users.noreply.github.com>
2023-03-24 17:59:06 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: 'https://emerald.tiwweb.workers.dev/',
+    bare: 'https://tiwmain.tiwweb.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
