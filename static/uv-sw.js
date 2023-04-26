721b313e849de75620510e1bc9cfef7294df341a
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-04-25 19:47:23 -0500

    Update uv-sw.js

--- static/uv-sw.js
+++ static/uv-sw.js
@@ -1,21 +1,5 @@
-var cacheName = 'TIWcog';
-var filesToCache = [
-  '/js/sw.js'
-];
+importScripts("/static/uv/uv.sw.js");
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
+const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
